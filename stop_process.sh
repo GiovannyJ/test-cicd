a5085ce9e017c42ccfc0833b522b3919be98e7dc
GiovannyJ <94592434+GiovannyJ@users.noreply.github.com>
2023-10-07 16:12:50 -0400

    update things

---
+++ stop_process.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Find and kill the process by name or other criteria
+# For example, if your Go application is named "app"
+# Replace "app" with the actual process name or criteria
+pkill -f "app"
+
+# You can add more commands if necessary
+# For example, to handle additional cleanup tasks
+# ...
+
+echo "Process stopped."
